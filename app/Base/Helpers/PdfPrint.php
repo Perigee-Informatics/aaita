1b2c9cff6f5af334358caf4d9cfd59d1cfec5a20
er.beeps <bipinkhatiwada10@gmail.com>
2022-11-04 19:36:38 +0545

    fixs

--- app/Base/Helpers/PdfPrint.php
+++ app/Base/Helpers/PdfPrint.php
@@ -11,14 +11,14 @@ class PdfPrint
         return self::print("HJ_UbfC4P", $content, $recipe, "none", $file_name);
     }
     public static function printPortrait($content, $file_name, $recipe = "chrome"){
-        return self::print("HJ_UbfC4P", $content, $recipe, "none", $file_name);
+        return self::print("HJe27fgQIw", $content, $recipe, "none", $file_name);
     }
     private static function loadJsReportConfig(){
         self::$jsreport_url= Config('report.jsreport_url');
     }
 
     public static function storeprintPortrait($content, $file_name, $recipe = "chrome-pdf"){
-        return self::store_print("HJ_UbfC4P", $content, $recipe, "none", $file_name);
+        return self::store_print("HJe27fgQIw", $content, $recipe, "none", $file_name);
     }
 
     public static function print($shortid, $content, $recipe, $engine, $file_name){
